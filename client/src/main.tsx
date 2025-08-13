de85ebffdc5992d0cdf69aab2c088dcb7579531a
xassdly <xassdly@gmail.com>
2025-08-13 12:37:10 +0200

    fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/flown'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
